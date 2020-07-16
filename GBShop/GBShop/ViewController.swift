2c75667be10d5d7026f63cb89b1897b925c8a4df
Aleksandr Fetisov <grashey@mail.ru>
2020-07-16 18:19:15 +0300

    GBShop proj started

---
+++ GBShop/GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Aleksandr Fetisov on 16.07.2020.
+//  Copyright © 2020 Aleksandr Fetisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
